4eddf7a4ff65343f992a6fbeffedee505e3e09e8
ShaniaB417 <107355668+ShaniaB417@users.noreply.github.com>
2024-02-15 14:58:28 -0500

    Nav Update
    updated search bar and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
 
         <div className="search-bar">
           <input type="text" placeholder="Search..." />
-          <button>Search</button>
+         
         </div>
 
         <div className="sign-up-login">
