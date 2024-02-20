b393ed93442e92bf1de642069f5c141d6299ff5c
MoraceA <99560179+MoraceA@users.noreply.github.com>
2024-02-19 23:32:48 -0500

    CreateAccount page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from '../src/pages/Home/HomePage'; // Assuming this is the path to your homepage
-
+import CreateAccount from '../src/pages/Authentication/CreateAccount'; 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+      <Route exact path="/" element={<Homepage />} />
+        <Route path="/home" element={<Homepage />} />
+        <Route exact path="/createaccount" element={<CreateAccount />} />
       </Routes>
     </Router>
   );
--- src/pages/Authentication/CreateAccount.jsx
+++ src/pages/Authentication/CreateAccount.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
+import './CreateAccount.css';
 
-
-
-
-const CreateAccountForm = () => {
+const CreateAccount = () => {
   const [formData, setFormData] = useState({
     firstname: '',
     lastname: '',
@@ -24,26 +22,38 @@ const CreateAccountForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-       First Name:
-        <input type="text" name="firstname" value={formData.name} onChange={handleChange} />
-      </label>
-      <label>
-       Last Name:
-        <input type="text" name="lastname" value={formData.name} onChange={handleChange} />
-      </label>
-      <label>
-        Email:
-        <input type="email" name="email" value={formData.email} onChange={handleChange} />
-      </label>
-      <label>
-        username:
-        <textarea name="username" value={formData.message} onChange={handleChange} />
-      </label>
-      <button type="submit">Create Account</button>
-    </form>
+    <div className="createaccount">
+      <h1>Join Now!</h1>
+      <p>Create an account to get started.</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+         First Name:
+          <input type="text" name="firstname" value={formData.firstname} onChange={handleChange} />
+        </label>
+        <label>
+         Last Name:
+          <input type="text" name="lastname" value={formData.lastname} onChange={handleChange} />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        </label>
+        <label>
+          Username:
+          <input type="text" name="username" value={formData.username} onChange={handleChange} />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        </label>
+        <label>
+          Confirm Password:
+          <input type="password" name="confirmpassword" value={formData.confirmpassword} onChange={handleChange} />
+        </label>
+        <button type="submit">Create Account</button>
+      </form>
+    </div>
   );
 };
 
-export default CreateAccountForm;
+export default CreateAccount;
