d1adc91141d1c14362f9e8f69084afe6fd061452
ShaniaB417 <107355668+ShaniaB417@users.noreply.github.com>
2024-02-21 23:28:38 -0500

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Homepage from '../src/pages/Home/HomePage';
 import CreateAccount from '../src/pages/Authentication/CreateAccount'; 
 import Login from '../src/pages/Authentication/Login'; 
 
+
 function App() {
   return (
     <Router>
@@ -12,17 +13,7 @@ function App() {
         <Route path="/home" element={<Homepage />} />
         <Route exact path="/createaccount" element={<CreateAccount />} />
         <Route exact path="/login" element={<Login />} />
-        <Route exact path="/about" element={<AboutUs/>} />
-        <Route exact path="/contact" element={<Contact/>} />
-        <Route exact path="/FAQ" element={<FAQ/>} />
-        <Route exact path="/profile" element={<Profile/>} />
-        <Route exact path="/kid's" element={<Kids/>} />
-        <Route exact path="/mens" element={<Mens/>} />
-        <Route exact path="/women's" element={<Womens/>} />
-        <Route exact path="/brands" element={<Brands/>} />
-
-
-        
+    
       </Routes>
     </Router>
   );
