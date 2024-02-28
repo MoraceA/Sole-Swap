0220d23a24df9b64b7801a7018cee97a5b85dcc2
MoraceA <99560179+MoraceA@users.noreply.github.com>
2024-02-28 00:11:36 -0500

    Create Account Firebase
    The user's information is stored in firebase when they create an account

--- firebase.js
+++ firebase.js
@@ -1,11 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyDFyBtrCITLqt5lsYWvdVDTwy8ZPCPPKFU",
   authDomain: "soleswap-d3aac.firebaseapp.com",
@@ -19,3 +18,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export {app, analytics, auth}
--- src/pages/Authentication/CreateAccount.jsx
+++ src/pages/Authentication/CreateAccount.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import './CreateAccount.css';
-
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const CreateAccount = () => {
-  const [formData, setFormData] = useState({
+  const [userCredentials, setUserCredentials] = useState({
     firstname: '',
     lastname: '',
     email: '',
@@ -13,45 +13,56 @@ const CreateAccount = () => {
   });
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setUserCredentials({ ...userCredentials, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission here, for example, submit the formData to a server
-    console.log(formData);
+    console.log(userCredentials); // Check the form data
+    // You can handle form submission logic here if needed
+  };
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    console.log('signup');
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, userCredentials.email, userCredentials.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user); // Successfully signed up user
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage); // Log any errors
+      });
   };
 
   return (
     <div className="createaccount">
-        <img src="src/assets/LoginPicture.png"/>
+      <img src="src/assets/LoginPicture.png" alt="Login" />
       <h1>Sign Up</h1>
       <form onSubmit={handleSubmit}>
         <label>
-    
-          <input type="text" name="firstname" id="firstname" className="input-field" placeholder="First Name" value={formData.firstname} onChange={handleChange} />
+          <input type="text" name="firstname" id="firstname" className="input-field" placeholder="First Name" value={userCredentials.firstname} onChange={handleChange} />
         </label>
         <label>
-       
-          <input type="text" name="lastname" id="lastname" className="input-field" placeholder="Last Name"value={formData.lastname} onChange={handleChange} />
+          <input type="text" name="lastname" id="lastname" className="input-field" placeholder="Last Name" value={userCredentials.lastname} onChange={handleChange} />
         </label>
         <label>
-      
-          <input type="email" name="email" id="email" className="input-field" placeholder="Email"value={formData.email} onChange={handleChange} />
+          <input type="email" name="email" id="email" className="input-field" placeholder="Email" value={userCredentials.email} onChange={handleChange} />
         </label>
         <label>
-    
-          <input type="text" name="username" id="username" className="input-field" placeholder="Username" value={formData.username} onChange={handleChange} />
+          <input type="text" name="username" id="username" className="input-field" placeholder="Username" value={userCredentials.username} onChange={handleChange} />
         </label>
         <label>
-          
-          <input type="password" name="password" id="password" className="input-field" placeholder="Password" value={formData.password} onChange={handleChange} />
+          <input type="password" name="password" id="password" className="input-field" placeholder="Password" value={userCredentials.password} onChange={handleChange} />
         </label>
         <label>
-         
-          <input type="password" name="confirmpassword" id="confirmpassword" className="input-field" placeholder="Confirm Password" value={formData.confirmpassword} onChange={handleChange} />
+          <input type="password" name="confirmpassword" id="confirmpassword" className="input-field" placeholder="Confirm Password" value={userCredentials.confirmpassword} onChange={handleChange} />
         </label>
-        <button type="submit" className="signup-button">Create Account</button>
+        <button type="submit" className="signup-button" onClick={handleSignUp}>Create Account</button>
       </form>
     </div>
   );
