7b254f63034cc6b313459e1ad64bfff7527cc922
varuar <116020359+varuar@users.noreply.github.com>
2024-02-29 09:29:38 -0500

    email

--- src/pages/About/Contact.jsx
+++ src/pages/About/Contact.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
+import './Contact.css';
+import soleSwapLogo from '../../assets/SOLE SWAP.png'; 
 import { Link } from 'react-router-dom';
-import './Contact.css'; 
-import soleSwapLogo from '../../assets/SOLE SWAP.png';
 
 function Contact() {
+  const sendEmail = (e) => {
+    e.preventDefault(); 
+    emailjs.sendForm('service_ted287k', 'template_2bdskeb', e.target, 'kIev1I-QO2hBowJKf')
+    .then((result) => {
+          console.log(result.text);
+          alert('Message sent successfully!');
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+          alert('Failed to send the message, please try again.');
+      });
+  };
+
   return (
     <div className="contact-container">
-      {/* Logo with Link to Home */}
       <Link to="/">
         <img src={soleSwapLogo} alt="SoleSwap Logo" className="soleSwap-logo" />
       </Link>
-      
+
       <h2>Contact Us</h2>
       <p>If you have any questions or inquiries, feel free to reach out to us using the form below:</p>
-      <form className="contact-form">
+      <form className="contact-form" onSubmit={sendEmail}>
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" placeholder="Your name..." />
+        <input type="text" id="name" name="name" placeholder="Your name..." required />
 
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" placeholder="Your email..." />
+        <input type="email" id="email" name="email" placeholder="Your email..." required />
 
         <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" rows="4" placeholder="Your message..."></textarea>
+        <textarea id="message" name="message" rows="4" placeholder="Your message..." required></textarea>
 
         <button type="submit">Send Message</button>
       </form>
