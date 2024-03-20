fb0cea36c7495da14b0daacf8690f40334898850
MoraceA <99560179+MoraceA@users.noreply.github.com>
2024-03-20 12:04:32 -0400

    Login Update

--- src/pages/Authentication/CreateAccount.jsx
+++ src/pages/Authentication/CreateAccount.jsx
@@ -69,34 +69,41 @@ const CreateAccount = () => {
 
   return (
     <div className="createaccount">
-      <img src="src/assets/LoginPicture.png" alt="Login" />
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <input type="text" name="firstname" id="firstname" className="input-field" placeholder="First Name" value={userCredentials.firstname} onChange={handleChange} />
-        </label>
-        <label>
-          <input type="text" name="lastname" id="lastname" className="input-field" placeholder="Last Name" value={userCredentials.lastname} onChange={handleChange} />
-        </label>
-        <label>
-          <input type="email" name="email" id="email" className="input-field" placeholder="Email" value={userCredentials.email} onChange={handleChange} />
-        </label>
-        <label>
-          <input type="text" name="username" id="username" className="input-field" placeholder="Username" value={userCredentials.username} onChange={handleChange} />
-        </label>
-        <label>
-          <input type="password" name="password" id="password" className="input-field" placeholder="Password" value={userCredentials.password} onChange={handleChange} />
-        </label>
-        <label>
-          <input type="password" name="confirmpassword" id="confirmpassword" className="input-field" placeholder="Confirm Password" value={userCredentials.confirmpassword} onChange={handleChange} />
-        </label>
-        <button type="submit" className="signup-button" onClick={handleSignUp}>Create Account</button>
-      </form>
-
-      {/* Error message */}
-      {error && <div className="error">{error}</div>}
+      <div className="section1">
+        <img src="src/assets/LoginPicture.png" alt="Login" />
+      </div>
+  
+      <div className="section2">
+        <h1>Sign Up</h1>
+  
+        <form onSubmit={handleSubmit}>
+          <label>
+            <input type="text" name="firstname" id="firstname" className="input-field" placeholder="First Name" value={userCredentials.firstname} onChange={handleChange} />
+          </label>
+          <label>
+            <input type="text" name="lastname" id="lastname" className="input-field" placeholder="Last Name" value={userCredentials.lastname} onChange={handleChange} />
+          </label>
+          <label>
+            <input type="email" name="email" id="email" className="input-field" placeholder="Email" value={userCredentials.email} onChange={handleChange} />
+          </label>
+          <label>
+            <input type="text" name="username" id="username" className="input-field" placeholder="Username" value={userCredentials.username} onChange={handleChange} />
+          </label>
+          <label>
+            <input type="password" name="password" id="password" className="input-field" placeholder="Password" value={userCredentials.password} onChange={handleChange} />
+          </label>
+          <label>
+            <input type="password" name="confirmpassword" id="confirmpassword" className="input-field" placeholder="Confirm Password" value={userCredentials.confirmpassword} onChange={handleChange} />
+          </label>
+          <button type="submit" className="signup-button" onClick={handleSignUp}>Create Account</button>
+        </form>
+  
+        {/* Error message */}
+        {error && <div className="error">{error}</div>}
+      </div>
     </div>
   );
 };
 
 export default CreateAccount;
+
