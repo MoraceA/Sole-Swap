c9a53ee59b54505bb20fc796c1e7497ac535c6c5
ShaniaB417 <107355668+ShaniaB417@users.noreply.github.com>
2024-03-21 12:06:30 -0400

    update pathway

--- src/pages/Home/user_HomePage.jsx
+++ src/pages/Home/user_HomePage.jsx
@@ -5,7 +5,7 @@ import 'slick-carousel/slick/slick-theme.css';
 import '../Home/home.css';
 import { signOut } from "firebase/auth";
 import { getAuth } from "firebase/auth"; // Import getAuth from firebase/auth
-import { auth } from '/Users/ariana/Documents/Sole-Swap/firebase.js';
+import { auth } from '/Users/shaniabrown/Documents/GitHub/Sole-Swap/src/firebase.js';
 
 function Homepage() {
   const carouselItems = [
