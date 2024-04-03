cea4f8235613f9e0d8badd390a4bc81d2f1a7cf8
ShaniaB417 <107355668+ShaniaB417@users.noreply.github.com>
2024-04-03 19:24:00 -0400

    deleted search bar

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -33,9 +33,7 @@ function Homepage() {
           <img src="src/assets/SOLE SWAP.png" alt="Sole Swap Logo" className="logo" />
         </div>
 
-        <div className="search-bar">
-          <input type="text" placeholder="Search..." />
-        </div>
+        
 
         <div className="sign-up-login">
           <button> <a href="/likedShoes">❤️</a></button>
