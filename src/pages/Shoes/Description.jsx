b1abe4ab2374279e5ae375cbbbfcb983ee42f196
MoraceA <99560179+MoraceA@users.noreply.github.com>
2024-04-11 09:45:49 -0400

    Login Bug Fixes

---
+++ src/pages/Shoes/Description.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import './Description.css'; // Import CSS file
+
+function Description() {
+  return (
+    <div className="figma-container">
+      {/* MacBook Air - 1 */}
+      <div id="macbook-air" className="macbook-air">
+        {/* Sole Swap */}
+        <div id="sole-swap" className="sole-swap">Sole Swap</div>
+        {/* Rectangle 1 */}
+        <div id="rectangle-1" className="rectangle-1"></div>
+        {/* Frame */}
+        <div className="frame"></div>
+        {/* Vectors */}
+        <div className="vector"></div>
+        <div className="vector"></div>
+        <div className="vector"></div>
+        <div className="vector"></div>
+        <div className="vector"></div>
+        <div className="vector"></div>
+        <div className="vector"></div>
+        <div className="vector"></div>
+        <div className="vector"></div>
+        {/* Search */}
+        <div id="search" className="search">Search</div>
+        {/* Nav Bar */}
+        <div className="nav-bar">
+          {/* Rectangle 2 */}
+          <div id="rectangle-2" className="rectangle-2"></div>
+          {/* Brands, Womens, Kids, Mens */}
+          <div className="brand-text">Brands</div>
+          <div className="brand-text">Womens</div>
+          <div className="brand-text">Kids</div>
+          <div className="brand-text">Mens</div>
+          {/* Vectors */}
+          <div className="vector"></div>
+          <div className="vector"></div>
+          <div className="vector"></div>
+          <div className="vector"></div>
+          <div className="vector"></div>
+          <div className="vector"></div>
+        </div>
+        {/* Air Jordan 1 Retro High OG UNC Toe */}
+        <div id="air-jordan" className="air-jordan">Air Jordan 1 Retro High OG UNC Toe</div>
+        {/* Size US 9 Used- Good Condition Air Jordan */}
+        <div id="size" className="size">Size US 9 Used- Good Condition Air Jordan</div>
+        {/* Rectangle 3 */}
+        <div id="rectangle-3" className="rectangle-3"></div>
+        {/* Make Offer */}
+        <div id="make-offer" className="make-offer">Make Offer</div>
+        {/* Rectangle 4 */}
+        <div id="rectangle-4" className="rectangle-4"></div>
+        {/* Add to Bag */}
+        <div id="add-to-bag" className="add-to-bag">Add to Bag</div>
+        {/* Rectangle 5 */}
+        <div id="rectangle-5" className="rectangle-5"></div>
+        {/* All purchases through Sole Swap are covered by Buyer Protection */}
+        <div id="buyer-protection" className="buyer-protection">All purchases through Sole Swap are covered by Buyer Protection.</div>
+        {/* Learn More */}
+        <div id="learn-more" className="learn-more">Learn More</div>
+        {/* Lines */}
+        <div className="line"></div>
+        <div className="line"></div>
+        {/* Description */}
+        <div id="description" className="description">Description Great Condition Mens size 9 No box</div>
+        {/* Ellipse 1 */}
+        <div id="ellipse" className="ellipse"></div>
+        {/* iluvjs */}
+        <div id="iluvjs" className="iluvjs">iluvjs</div>
+        {/* Stars */}
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        {/* (503) */}
+        <div id="503" className="503">(503)</div>
+        {/* Rectangle 6 */}
+        <div id="rectangle-6" className="rectangle-6"></div>
+        {/* Rectangle 7 */}
+        <div id="rectangle-7" className="rectangle-7"></div>
+        {/* Visit Profile */}
+        <div id="visit-profile" className="visit-profile">Visit Profile</div>
+        {/* Chat */}
+        <div id="chat" className="chat">Chat</div>
+      </div>
+    </div>
+  );
+}
+
+export default Description;
